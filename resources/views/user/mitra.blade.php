453a9dfaa5ac3b8e0a654e46f87193f7b75fca73
unknown <axsenathanzx@gmail.com>
2025-10-20 11:31:40 +0700

    mitra

---
+++ resources/views/user/mitra.blade.php
@@ -0,0 +1,39 @@
+@extends('welcome')
+
+@section('content')
+    <section class="mitra-section">
+        <h2>Mitra Kerjasama</h2>
+
+        <div class="mitra-container">
+            <div class="mitra-card">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/ACN_Newswire_logo.png/320px-ACN_Newswire_logo.png"
+                    alt="ACN News Wire">
+                <div class="mitra-info">
+                    <h3>ACN News Wire</h3>
+                    <p>ACN News Wire menyediakan jasa penyebaran press release secara real time bagi perusahaan dan
+                        organisasi yang ingin menyiarkan dan menyebarkan informasi kepada media, bisnis, dan pasar keuangan
+                        di seluruh dunia. ACN mendistribusikan siaran pers dalam berbagai bahasa.</p>
+                </div>
+            </div>
+
+            <div class="mitra-card">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Agence_France-Presse_logo.svg" alt="AFP">
+                <div class="mitra-info">
+                    <h3>AFP</h3>
+                    <p>AFP adalah kantor berita berkelas dunia yang memberikan liputan cepat dan mendalam atas beragam
+                        konflik politik, olahraga, hiburan, dan teknologi terkini.</p>
+                </div>
+            </div>
+
+            <div class="mitra-card">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Associated_Press_logo_2012.svg"
+                    alt="Associated Press">
+                <div class="mitra-info">
+                    <h3>Associated Press</h3>
+                    <p>Associated Press dibentuk pada tahun 1846. Dengan jangkauan lebih dari 100 negara, AP menyampaikan
+                        berita yang dibaca oleh jutaan orang di seluruh dunia setiap harinya.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
